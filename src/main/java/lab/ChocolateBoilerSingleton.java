1ebb27b83bb2eef16d8b93831f6d7d87e8bdb58b
Mauricio <mauricio.pinto@utec.edu.pe>
2020-10-07 09:30:08 -0500

    singleton

---
+++ src/main/java/lab/ChocolateBoilerSingleton.java
@@ -0,0 +1,40 @@
+package src.main.java.lab;
+
+public class ChocolateBoilerSingleton {
+    private boolean empty;
+    private boolean boiled;
+
+    public ChocolateBoilerSingleton() {
+        empty = true;
+        boiled = false;
+    }
+
+    public void fill(){
+        if(isEmpty()){
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain(){
+        if(isEmpty() && isBoiled()){
+            //drain the boiled milk and chocolate
+            empty = true;
+        }
+    }
+
+    public void boil(){
+        if(!isEmpty() && isBoiled()){
+            //bring the contents to a boil
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty(){
+        return empty;
+    }
+
+    public boolean isBoiled(){
+        return boiled;
+    }
+}
